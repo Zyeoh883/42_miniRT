5bdf9ad4713c30945953c079ddea0b77866bd0be
Zi Shen Yeoh <zyeoh@u81z04s01.42kl.edu.my>
2024-06-18 15:24:33 +0800

    feat: Added camera struct
    Located in data_struct.h

--- inc/data_struct.h
+++ inc/data_struct.h
@@ -6,27 +6,19 @@
 /*   By: zyeoh <zyeoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 19:16:07 by zyeoh             #+#    #+#             */
-/*   Updated: 2024/06/14 11:19:45 by zyeoh            ###   ########.fr       */
+/*   Updated: 2024/06/18 15:20:52 by zyeoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DATA_STRUCT_H
 # define DATA_STRUCT_H
 
-typedef struct s_article
+typedef struct s_camera
 {
-	char	*title;
-	char	*author;
-	char	*body;
-	int		no_comments;
-	int		no_claps;
-
-} __attribute__((packed)) t_article;
-
-typedef struct s_vector
-{
-	int		vector[4];
-}			t_vector;
+	float	position[3];
+	float	quad[4];
+	float	fov;
+}			t_camera;
 
 // __attribute__((packed,aligned(4))
 // __attribute__((packed))
