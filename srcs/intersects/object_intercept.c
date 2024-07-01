f848e08ee64a03a40b10ea5e879ca1a3d053a973
Zi Shen Yeoh <zyeoh@u81z02s01.42kl.edu.my>
2024-07-01 15:50:45 +0800

    chore: saving state for fixing aabb intersection

--- srcs/intersects/object_intercept.c
+++ srcs/intersects/object_intercept.c
@@ -6,11 +6,46 @@
 /*   By: zyeoh <zyeoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:13:56 by zyeoh             #+#    #+#             */
-/*   Updated: 2024/06/30 21:41:06 by zyeoh            ###   ########.fr       */
+/*   Updated: 2024/07/01 15:43:18 by zyeoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+
+// // r.dir is unit direction vector of ray
+// dirfrac.x = 1.0f / r.dir.x;
+// dirfrac.y = 1.0f / r.dir.y;
+// dirfrac.z = 1.0f / r.dir.z;
+// // lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
+// // r.org is origin of ray
+// float t1 = (lb.x - r.org.x)*dirfrac.x;
+// float t2 = (rt.x - r.org.x)*dirfrac.x;
+// float t3 = (lb.y - r.org.y)*dirfrac.y;
+// float t4 = (rt.y - r.org.y)*dirfrac.y;
+// float t5 = (lb.z - r.org.z)*dirfrac.z;
+// float t6 = (rt.z - r.org.z)*dirfrac.z;
+
+// float tmin = max(max(min(t1, t2), min(t3, t4)), min(t5, t6));
+// float tmax = min(min(max(t1, t2), max(t3, t4)), max(t5, t6));
+
+// // if tmax < 0, ray (line) is intersecting AABB, but the whole AABB is behind us
+// if (tmax < 0)
+// {
+//     t = tmax;
+//     return false;
+// }
+
+// // if tmin > tmax, ray doesn't intersect AABB
+// if (tmin > tmax)
+// {
+//     t = tmax;
+//     return false;
+// }
+
+// t = tmin;
+// return true;
+
 
 // TODO validate aabb SIMB SSE
 int	intersect_aabb(t_ray *ray, t_vector max, t_vector min)
@@ -23,8 +58,8 @@ int	intersect_aabb(t_ray *ray, t_vector max, t_vector min)
 
 	// mask = _mm_max_ps(ray->direction, _mm_set1_ps(1e-8f));
 
-	t0 = _mm_div_ps(min, ray->direction);
-    t1 = _mm_div_ps(max, ray->direction);
+	t0 = _mm_div_ps(min - *ray->pos, ray->direction);
+    t1 = _mm_div_ps(max - *ray->pos, ray->direction);
 
 	tmin = _mm_min_ps(t0, t1);
    	tmax = _mm_max_ps(t0, t1);
@@ -80,33 +115,33 @@ int	intersect_obb(t_ray *ray, t_OBB obb)
 }
 
 
-// int	intersect_aabb(t_ray ray, t_vector max, t_vector min)
-// {
-// 	float	t[2];
-// 	float	temp[2];
+int	intersect_aabb(t_ray ray, t_vector max, t_vector min)
+{
+	float	t[2];
+	float	temp[2];
 
-// 	t[0] = (min.i - ray.pos->i) / ray.direction.i;
-// 	t[1] = (max.i - ray.pos->i) / ray.direction.i;
-// 	if (t[0] > t[1])
-// 		swap_floats(&t[1], &t[0]);
-// 	temp[0] = (min.j - ray.pos->j) / ray.direction.j;
-// 	temp[1] = (max.j - ray.pos->j) / ray.direction.j;
-// 	if (temp[0] > temp[1])
-// 		swap_floats(&temp[0], &temp[1]);
-// 	if (t[0] > temp[1] || temp[0] > t[1])
-// 		return (false);
-// 	if (temp[0] > t[0])
-// 		t[0] = temp[0];
-// 	if (temp[1] > t[1])
-// 		t[1] = temp[1];
-// 	temp[0] = (min.k - ray.pos->k) / ray.direction.k;
-// 	temp[1] = (max.k - ray.pos->k) / ray.direction.k;
-// 	if (temp[0] > temp[1])
-// 		swap_floats(&temp[0], &temp[1]);
-// 	if (t[0] > temp[1] || temp[0] > t[1])
-// 		return (false);
-// 	return (t[1] > 0 || temp[1] > 0);
-// }
+	t[0] = (min.i - ray.pos->i) / ray.direction.i;
+	t[1] = (max.i - ray.pos->i) / ray.direction.i;
+	if (t[0] > t[1])
+		swap_floats(&t[1], &t[0]);
+	temp[0] = (min.j - ray.pos->j) / ray.direction.j;
+	temp[1] = (max.j - ray.pos->j) / ray.direction.j;
+	if (temp[0] > temp[1])
+		swap_floats(&temp[0], &temp[1]);
+	if (t[0] > temp[1] || temp[0] > t[1])
+		return (false);
+	if (temp[0] > t[0])
+		t[0] = temp[0];
+	if (temp[1] > t[1])
+		t[1] = temp[1];
+	temp[0] = (min.k - ray.pos->k) / ray.direction.k;
+	temp[1] = (max.k - ray.pos->k) / ray.direction.k;
+	if (temp[0] > temp[1])
+		swap_floats(&temp[0], &temp[1]);
+	if (t[0] > temp[1] || temp[0] > t[1])
+		return (false);
+	return (t[1] > 0 || temp[1] > 0);
+}
 
 // int	intersect_obb(t_ray ray, float t[2])
 // {
